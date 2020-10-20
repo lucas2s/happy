16b281ade023ea8fac2eb83fcab8b88ba7ee37fb
Lucas Silva Sartori <sartori.s.lucas@gmail.com>
2020-10-20 18:40:52 -0300

    Adicionando Variáveis de ambiente

--- backend/src/views/images_view.ts
+++ backend/src/views/images_view.ts
@@ -4,7 +4,7 @@ export default {
   render(image:Image ) {
     return {
       id: image.id,
-      url: `http://localhost:3333/tmp/uploads/${image.path}`,
+      url: process.env.APP_URL_UPLOADS + image.path,
     };
   },
 
