f0f9f81316febfef43be8261f2ad95e49d21baab
Lucas Silva Sartori <sartori.s.lucas@gmail.com>
2020-10-20 01:08:33 -0300

    Criado banco de dados e criado rota create orphanage

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,40 @@
 import express from 'express';
+import { getRepository } from 'typeorm'
+
+import Orphanage from './models/Orphanage';
+import './database/connection';
 
 const app = express();
+
+app.use(express.json());
+
+app.post('/orphanages', async (request, reponse) => {
+
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  } = request.body;
+
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  });
+
+  await orphanagesRepository.save(orphanage);
+
+  return reponse.status(201).json(orphanage);
+});
 
 app.listen(3333);
